1453743033034e2ac2298abc25d992c0752ef424
luka-dv <luka.guva@gmail.com>
2025-09-03 12:00:58 +0200

    add middleware and get logic

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,21 @@
+import express, { Request, Response } from "express";
 
-import express from "express";
-
-const PORT = process.env.PORT;
-
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 
-console.log(PORT);
 
-app.listen(PORT)
+app.use("/app", express.static("./src/app"));
+
+
+app.get("/healthz", handlerReadiness);
+
+async function handlerReadiness(req: Request, res: Response): Promise<void> {
+    res.set("Content-Type", "text/plain; charset=utf-8");
+    res.send("OK");
+}
+
+
+app.listen(PORT, () => {
+    console.log(`Bravo! Server is listening on PORT: ${PORT}.`);
+})
