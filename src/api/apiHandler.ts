fc3ad5654396c867856a95853d9e6ea5cdb76445
luka-dv <luka.guva@gmail.com>
2025-09-17 11:39:14 +0200

    update folder structure

--- src/api/apiHandler.ts
+++ src/api/apiHandler.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 
-
 export async function handlerReadiness(_: Request, res: Response): Promise<void> {
     res.set("Content-Type", "text/plain; charset=utf-8");
     res.send("OK");
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import { countFileserverHits, middlewareLogResponses } from "./middleware.js";
 import { handlerReadiness} from "./api/apiHandler.js";
-import { adminView, resetNumOfRequests } from "./admin/adminHandlers.js";
+import { adminView, resetNumOfRequests } from "./api/adminHandlers.js";
 
 const PORT = process.env.PORT || 8080;
 
@@ -13,10 +13,10 @@ app.use(middlewareLogResponses);
 app.use("/app", countFileserverHits,express.static("./src/app")); // root is relative to the project root 
 
 app.get("/api/healthz", handlerReadiness);
-//app.get("/api/metrics", handlerNumOfRequests);
-app.get("/admin/reset", resetNumOfRequests);
 
 app.get("/admin/metrics", adminView);
+app.get("/admin/reset", resetNumOfRequests);
+
 
 
 app.listen(PORT, () => {
