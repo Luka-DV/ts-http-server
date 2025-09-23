65d28a450027e4756edc7ea6f6d4689f9a0d1243
luka-dv <luka.guva@gmail.com>
2025-09-23 11:52:17 +0200

    add postgres and drizzle

--- src/config.ts
+++ src/config.ts
@@ -1,8 +1,27 @@
 
+import { loadEnvFile } from 'node:process';
+loadEnvFile();
+
+
+const dbURL = envOrThrow("DB_URL")
+
+function envOrThrow(key: string) {
+    if(!process.env[key]) {
+        throw new Error("Missing connection string!");
+    }
+    return process.env[key];
+}
+
+
 export type APIConfig = {
     fileserverHits: number;
+    dbURL: string;
 };
 
 export const config: APIConfig = {
-    fileserverHits: 0
+    fileserverHits: 0,
+    dbURL: dbURL
 }
+
+
+
