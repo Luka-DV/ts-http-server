8705e9f2883ecd87a7f3a0678bb0959d9330162d
luka-dv <luka.guva@gmail.com>
2025-10-23 11:41:52 +0200

    add tests for JWTs

--- src/auth.test.ts
+++ src/auth.test.ts
@@ -1,6 +1,8 @@
 
-import { describe, it, expect, beforeAll } from "vitest";
+import { describe, it, expect, beforeAll, test } from "vitest";
 import { hashPassword, checkPasswordHash, makeJWT, validateJWT } from "./auth.js";
+import { UnauthorizedError } from "./errors.js";
+
 
 describe("Password Hashing", () => {
   const password1 = "correctPassword123!";
@@ -22,8 +24,89 @@ describe("Password Hashing", () => {
     const result = await checkPasswordHash(password1, hash2);
     expect(result).toBe(false);
   })
+
+  it("should return false for an empty password", async () => {
+    const result = await checkPasswordHash("", hash1);
+    expect(result).toBe(false);
+  });
   
 });
 
-//Make sure that you can create and validate JWTs, that expired tokens are rejected, and JWTs signed with the wrong secret are rejected.
 
+//Create and validate JWTs, JWTs signed with the wrong secret are rejected and expired tokens are rejected.1
+
+describe("Creating and validating JWTs", () => {
+
+    const userID1 = "potato";
+    const userID2 = "cucumber";
+    let jwt1: string;
+    let jwt2: string;
+    let expiredJwt3: string;
+    const secret1 = "shhh";
+    const secret2 = "blue";
+
+    beforeAll(() => {
+        jwt1 = makeJWT(userID1, 5, secret1);
+        jwt2 = makeJWT(userID2, 10, secret2);
+        expiredJwt3 = makeJWT(userID1, 0, secret1)
+    })
+
+    test("should return the correct payload(userID) for the right secret", () => {
+        const payload = validateJWT(jwt1, secret1);
+        expect(payload).toBe(userID1);
+    })
+
+    test("should throw right error when signed with wrong secret", () => {
+        expect(() => validateJWT(jwt1, secret2)).toThrowError(expect.objectContaining({
+            message: 'invalid signature',
+        }))
+    })
+
+     /* This also works:
+    > constructor match 
+        expect(() => validateJWT(jwt1, secret2)).toThrowError(UnauthorizedError)
+
+    > regex string message match
+        expect(() => validateJWT(jwt1, secret2)).toThrowError(/invalid signature/);
+    */
+
+    setTimeout(() => {}, 50);
+
+    test("should throw right error if token expires", () => {
+        expect(() => validateJWT(expiredJwt3, secret1)).toThrowError(expect.objectContaining({
+            message: 'Token has expired',
+        }))
+    })
+});
+
+
+
+
+
+/* Vitest docs examples
+
+function getFruitStock(type: string) {
+  if (type === 'pineapples') {
+    throw new Error('Pineapples are not in stock')
+  }
+
+  // Do some other stuff
+}
+
+test('throws on pineapples', () => {
+  // Test that the error message says "stock" somewhere: these are equivalent
+  expect(() => getFruitStock('pineapples')).toThrowError(/stock/)
+  expect(() => getFruitStock('pineapples')).toThrowError('stock')
+
+  // Test the exact error message
+  expect(() => getFruitStock('pineapples')).toThrowError(
+    /^Pineapples are not in stock$/,
+  )
+
+  expect(() => getFruitStock('pineapples')).toThrowError(
+    new Error('Pineapples are not in stock'),
+  )
+  expect(() => getFruitStock('pineapples')).toThrowError(expect.objectContaining({
+    message: 'Pineapples are not in stock',
+  }))
+}) */
