799a1ea1d06eca673d4abda4ef78c4332be3dc4a
luka-dv <luka.guva@gmail.com>
2026-04-06 13:05:30 +0200

    refactor and add ConflictError class

--- src/errors.ts
+++ src/errors.ts
@@ -1,25 +1,35 @@
 
-
 export class NotFoundError extends Error {
     constructor(text: string) {
         super(text);
+        this.name = "NotFoundError";
     }
 }
 
 export class ForbiddenError extends Error {
     constructor(text: string) {
         super(text);
+        this.name = "ForbiddenError";
     }
 }
 
 export class UnauthorizedError extends Error {
     constructor(text: string) {
         super(text);
+        this.name = "UnauthorizedError";
     }
 }
 
 export class BadRequestError extends Error {
     constructor(text: string) {
         super(text);
+        this.name = "BadRequestError";
+    }
+}
+
+export class ConflictError extends Error {
+    constructor(text: string) {
+        super(text);
+        this.name = "ConflictError";
     }
 }
