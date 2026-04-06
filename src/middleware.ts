4b0abd55f15a36f6cbef4a1bd6b25180bce1add0
luka-dv <luka.guva@gmail.com>
2026-04-06 13:07:14 +0200

    refactor errorHandler and import type

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,6 @@
-import { NextFunction, Request, Response } from "express";
+import type { NextFunction, Request, Response } from "express";
 import { config } from "./config.js";
-import { BadRequestError, ForbiddenError, NotFoundError, UnauthorizedError } from "./errors.js";
+import { BadRequestError, ConflictError, ForbiddenError, NotFoundError, UnauthorizedError } from "./errors.js";
 
 export function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
     res.on("finish", () => {
@@ -29,20 +29,27 @@ export function errorHandler(
     _next: NextFunction
 ) {
     console.error("Error handler: ", err);
+    let statusCode = 500;
+
     switch(true) {
+        case err instanceof ConflictError:
+            statusCode = 409;
+            break
         case err instanceof NotFoundError:
-            res.status(404).json({"error": err.message});
-            break;
+            statusCode = 404;
+            break
         case err instanceof ForbiddenError:
-            res.status(403).json({"error": err.message});
-            break;
+            statusCode = 403;
+            break
         case err instanceof UnauthorizedError:
-            res.status(401).json({"error": err.message});
-            break;
+            statusCode = 401;
+            break
         case err instanceof BadRequestError:
-            res.status(400).json({"error": err.message});
-            break;
-        default:
-            res.status(500).send("Internal Server Error");  
-    }
+            statusCode = 400;
+            break
+    } 
+
+    const errorMsg = statusCode === 500 ? "Internal server error" : err.message;
+
+    res.status(statusCode).json({"error": errorMsg});  
 }
